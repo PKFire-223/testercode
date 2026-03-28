11750fe46580ba2ffaf4320e93f17361725d96e9
PKFire-223 <nhatquang2703@gmail.com>
2026-03-28 23:30:26 +0700

    a

--- Lab3-1/app/(auth)/_layout.tsx
+++
@@ -1,5 +0,0 @@
-// 23521276 Bùi Trương Nhật Quang
-import { Stack } from "expo-router";
-export default function AuthLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
-}
--- Lab3-2/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],   // SDK 50+ đã include expo-router preset
-    // plugins: [require.resolve('expo-router/babel')] // <- KHÔNG CẦN nữa
-  };
-};
